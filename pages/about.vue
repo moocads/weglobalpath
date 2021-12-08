30b4bc624e59a4b5f154b8065ef2b968ec588f8b
Rui Wang-Dong <rtdong9421@gmail.com>
2021-12-08 15:21:58 -0500

    completed about page

--- pages/about.vue
+++ pages/about.vue
@@ -6,68 +6,68 @@
       </div>
     </header>
     <section class="sec-1 about-sec">
-      <div class="wrapper">
-        <div class="row-grid">
-          <div class="info-wrap">
-            <div class="title">
-              <img src="/img/logos/logo-small-white.png" alt="" />
-              <h2>加彼岸出国咨询</h2>
-            </div>
-            <p>
-              <span
-                >加彼岸旗下有加彼岸出国咨询（BEYONDCANADACONSULTING），彼森教育（BISONEDUCATION）和彼岸投资（BEYONDCANADAINVESTMENT）三家子公司</span
-              >，均为加拿大联邦注册公司，分别由自身移民律师，RCIC（加拿大移民顾问监管委员会）持牌移民顾问，教育顾问，职业培训师，地产经纪，会计师及商务专家组成的强大专业服务团队。
-            </p>
-            <p>
-              加彼岸出国咨询是权威办理加拿大省提名移民项目的加拿大本地公司。作为加拿大项目方，我们保持独家项目优势，始终保持高市场份额占有率，每位顾问都有多年的在海外的留学生活经验及海外工作经验。最重要的是，我们把每一位客户当作自己亲人来服务，在全加拿大境内的省提名项目中协助您评估现实状况并作出最合适的规划建议。加彼岸出国咨询将一如既往地与国内合作伙伴紧密合作，客户至上的精神，为客户提供最优质最专业的移民咨询服务。
-            </p>
+      <div class="row-grid">
+        <div class="info-wrap">
+          <div class="title">
+            <img src="/img/logos/logo-small-white.png" alt="" />
+            <h2>加彼岸出国咨询</h2>
           </div>
-          <figure>
-            <img src="/img/About/sec1.png" alt="" class="sec-img" />
-          </figure>
+          <p>
+            <span
+              >加彼岸旗下有加彼岸出国咨询（BEYONDCANADACONSULTING），彼森教育（BISONEDUCATION）和彼岸投资（BEYONDCANADAINVESTMENT）三家子公司</span
+            >，均为加拿大联邦注册公司，分别由自身移民律师，RCIC（加拿大移民顾问监管委员会）持牌移民顾问，教育顾问，职业培训师，地产经纪，会计师及商务专家组成的强大专业服务团队。
+          </p>
+          <p>
+            加彼岸出国咨询是权威办理加拿大省提名移民项目的加拿大本地公司。作为加拿大项目方，我们保持独家项目优势，始终保持高市场份额占有率，每位顾问都有多年的在海外的留学生活经验及海外工作经验。最重要的是，我们把每一位客户当作自己亲人来服务，在全加拿大境内的省提名项目中协助您评估现实状况并作出最合适的规划建议。加彼岸出国咨询将一如既往地与国内合作伙伴紧密合作，客户至上的精神，为客户提供最优质最专业的移民咨询服务。
+          </p>
+        </div>
+        <div class="imgContainer">
+          <img src="/img/About/sec1.png" alt="" class="sec-img" />
         </div>
       </div>
+      <!-- <div class="wrapper">
+      </div> -->
     </section>
     <section class="sec-2 about-sec">
-      <div class="wrapper">
-        <div class="row-grid">
-          <figure>
-            <img src="/img/About/sec2.png" alt="" class="sec-img" />
-          </figure>
-          <div class="info-wrap">
-            <div class="title">
-              <img src="/img/logos/logo-small-blue.png" alt="" />
-              <h2>彼森教育</h2>
-            </div>
-            <p>
-              <span>彼森教育</span
-              >致力于为中国的学子提供高质量的海外留学申请，出国考试咨询及职业规划咨询服务。自2018年与加拿大罗伯森学院深度合作起，我们已为上百名学生办理签证至拿到枫叶卡的所有手续。2021年加彼岸受加拿大罗伯森学院委托，是中国区唯一校方直属合作伙伴，为学校处理一切中国事宜。
-            </p>
-            <p>
-              结合加彼岸移居咨询方面和职业培训方面的超强实力，加拿大罗伯森学院与加彼岸出国咨询联合推出的公立学院直通车项目已成为广大留学生快速获得就业机会及移居身份的最佳选择。该项目不仅适合于即将高中或职高毕业的莘莘学子，对于过去已经完成过大学或大专课程，或已有多年工作经验的群体来说，该项目亦是您获得加拿大就业移居身份的绝佳途径。
-            </p>
+      <div class="row-grid">
+        <div class="imgContainer">
+          <img src="/img/About/sec2.png" alt="" class="sec-img" />
+        </div>
+        <div class="info-wrap">
+          <div class="title">
+            <img src="/img/logos/logo-small-blue.png" alt="" />
+            <h2>彼森教育</h2>
           </div>
+          <p>
+            <span>彼森教育</span
+            >致力于为中国的学子提供高质量的海外留学申请，出国考试咨询及职业规划咨询服务。自2018年与加拿大罗伯森学院深度合作起，我们已为上百名学生办理签证至拿到枫叶卡的所有手续。2021年加彼岸受加拿大罗伯森学院委托，是中国区唯一校方直属合作伙伴，为学校处理一切中国事宜。
+          </p>
+          <p>
+            结合加彼岸移居咨询方面和职业培训方面的超强实力，加拿大罗伯森学院与加彼岸出国咨询联合推出的公立学院直通车项目已成为广大留学生快速获得就业机会及移居身份的最佳选择。该项目不仅适合于即将高中或职高毕业的莘莘学子，对于过去已经完成过大学或大专课程，或已有多年工作经验的群体来说，该项目亦是您获得加拿大就业移居身份的绝佳途径。
+          </p>
         </div>
       </div>
+      <!-- <div class="wrapper">
+      </div> -->
     </section>
     <section class="sec-3 about-sec">
-      <div class="wrapper">
-        <div class="row-grid">
-          <div class="info-wrap">
-            <div class="title">
-              <img src="/img/logos/logo-small-blue.png" alt="" />
-              <h2>彼岸投资</h2>
-            </div>
-            <p>
-              <span>彼岸投资</span
-              >一直致力于为投资者提供专业的海外投资咨询服务。从投资学的角度来说，投资需要对市场深入了解，全面调查，综合行业动向，经济因素，风险与回报等多方面考量，以确保客户获得该行业必须提供的最佳服务和最真实的建议，找到最优的投资方案。
-            </p>
+      <div class="row-grid">
+        <div class="info-wrap">
+          <div class="title">
+            <img src="/img/logos/logo-small-blue.png" alt="" />
+            <h2>彼岸投资</h2>
           </div>
-          <figure>
-            <img src="/img/About/sec3.png" alt="" class="sec-img" />
-          </figure>
+          <p>
+            <span>彼岸投资</span
+            >一直致力于为投资者提供专业的海外投资咨询服务。从投资学的角度来说，投资需要对市场深入了解，全面调查，综合行业动向，经济因素，风险与回报等多方面考量，以确保客户获得该行业必须提供的最佳服务和最真实的建议，找到最优的投资方案。
+          </p>
+        </div>
+        <div class="imgContainer">
+          <img src="/img/About/sec3.png" alt="" class="sec-img" />
         </div>
       </div>
+      <!-- <div class="wrapper">
+      </div> -->
     </section>
     <section class="divider">
       <h3>连接彼岸，用心服务，帮助海外客户和学生。</h3>
@@ -76,9 +76,9 @@
     <section class="sec-4">
       <div class="wrapper">
         <div class="row-grid">
-          <figure>
+          <div class="imgContainer">
             <img src="/img/About/sec4.png" alt="" />
-          </figure>
+          </div>
           <article>
             <h2>我们的使命</h2>
             <p>脱离传统教育的“生存为本”，给所有人选择 国际化教育的机会。</p>
@@ -95,12 +95,70 @@
         </div>
       </div>
     </section>
+    <section class="sec-5">
+      <div class="wrapper">
+        <MainTitle title="加彼岸历程" titleEN="History" />
+        <a-timeline id="aboutTimeline" mode="alternate">
+          <a-timeline-item v-for="(item, index) in steps" :key="index">
+            <div slot="dot" class="dot">
+              <!-- <span>{{ item.label }}</span> -->
+              <div class="circle"></div>
+            </div>
+            <p class="label">{{item.label}}</p>
+            <p class="text">{{ item.step }}</p>
+          </a-timeline-item>
+        </a-timeline>
+      </div>
+    </section>
     <!-- <Construction /> -->
   </div>
 </template>
 
 <script>
-export default {};
+export default {
+  data() {
+    return {
+      steps: [
+        {
+          label: '加彼岸™历程: 1',
+          step: '建立中国与北美第一家鲁班工坊桥梁'
+        },
+        {
+          label: '加彼岸™历程: 15',
+          step: '缔结多所中加友好学校'
+        },
+        {
+          label: '加彼岸™历程: 98.1',
+          step: '98.1%+的移民申请通过率'
+        },
+        {
+          label: '加彼岸™历程: 100',
+          step: '代理加拿大百所院校'
+        },
+        {
+          label: '加彼岸™历程: 1000',
+          step: '1000+海外优质雇主'
+        },
+        {
+          label: '加彼岸™历程: 2000',
+          step: '输送上千名学子海外深造'
+        },
+        {
+          label: '加彼岸™历程: 6000',
+          step: '6000+各类移民申请成功案例'
+        },
+        {
+          label: '加彼岸™历程:',
+          step: '实现海外资产配置'
+        },
+        {
+          label: '加彼岸™历程:',
+          step: '未来给所有人选择国际化教育的机会'
+        },
+      ]
+    }
+  }
+};
 </script>
 
 <style lang="scss" scoped>
@@ -108,7 +166,7 @@ header {
   background-image: url("/img/About/banner-bw.png");
   background-color: rgba(34, 52, 92, 0.5);
   // filter: saturate(0%);
-  background-blend-mode: overlay;
+  background-blend-mode: multiply;
   background-repeat: no-repeat;
   background-size: cover;
   height: 400px;
@@ -138,7 +196,8 @@ header {
 }
 .sec-img {
   width: 100%;
-  height: auto;
+  height: 100%;
+  // height: auto;
   object-fit: cover;
 }
 .about-sec .row-grid {
@@ -148,6 +207,7 @@ header {
     display: flex;
     flex-direction: column;
     justify-content: center;
+    padding: 30px 0;
   }
   .title {
     display: flex;
@@ -168,9 +228,9 @@ header {
     font-size: 16px;
     line-height: 1.9;
   }
-  figure {
-    margin: 0;
-  }
+  // .imgContainer {
+  //   margin: 0;
+  // }
 }
 .sec-1 {
   background-color: $navy;
@@ -178,6 +238,7 @@ header {
     grid-template-columns: 65% 35%;
     .info-wrap {
       padding-right: 80px;
+      padding-left: 7.5vw;
     }
     p {
       color: #c4c4c4;
@@ -199,6 +260,7 @@ header {
     }
     .info-wrap {
       padding-left: 80px;
+      padding-right: 7.5vw;
     }
   }
   p {
@@ -214,12 +276,12 @@ header {
   display: grid;
   grid-template-columns: auto auto;
   .info-wrap {
-    padding: 80px 0;
     height: 100%;
     display: flex;
     flex-direction: column;
     justify-content: center;
     padding-right: 80px;
+    padding-left: 7.5vw;
   }
   .title {
     display: flex;
@@ -244,7 +306,7 @@ header {
     font-weight: bold;
     color: $navy;
   }
-  figure img {
+  .imgContainer img {
     object-fit: cover;
   }
 }
@@ -263,45 +325,59 @@ header {
     font-weight: 300;
   }
 }
-.sec-4 .row-grid {
-  display: grid;
-  grid-template-columns: 1fr 1fr;
-  align-items: center;
-  figure {
-    margin: 0;
-    img {
-      width: 100%;
-      height: auto;
-      object-fit: cover;
+.sec-4 {
+  // background-color: #e9e9e9;
+
+  .row-grid {
+    display: grid;
+    grid-template-columns: 1fr 1fr;
+    align-items: center;
+    .imgContainer {
+      margin: 0;
+      img {
+        width: 100%;
+        height: auto;
+        object-fit: cover;
+      }
     }
-  }
-  article {
-    h2 {
-      color: $navy;
-      font-size: 28px;
-      margin-bottom: 15px;
-      font-weight: bold;
-    }
-    p {
-      color: $navy;
-      font-size: 16px;
-      font-weight: bold;
-    }
-    ol {
-      list-style-type: decimal;
-      list-style-position: inside;
-      li {
-        color: #757575;
+    article {
+      h2 {
+        color: $navy;
+        font-size: 28px;
+        margin-bottom: 15px;
+        font-weight: bold;
+      }
+      p {
+        color: $navy;
         font-size: 16px;
+        font-weight: bold;
+      }
+      ol {
+        list-style-type: decimal;
+        list-style-position: inside;
+        li {
+          color: #757575;
+          font-size: 16px;
+        }
       }
     }
   }
 }
 
-@media all and (max-width: 1000px) {
+.sec-5 {
+  padding-bottom: 50px;
+  
+  .wrapper {
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    flex-direction: column;
+  }
+}
+
+@media all and (max-width: 991px) {
   header {
     background-position: bottom;
-    background-blend-mode: multiply;
     height: 300px;
     display: flex;
     align-items: center;
@@ -319,11 +395,11 @@ header {
   .sec-1,
   .sec-2,
   .sec-3 {
-    padding: 30px 0;
+    // padding: 30px 0;
     .row-grid {
       grid-template-columns: 1fr;
       .info-wrap {
-        padding: 0;
+        padding: 40px 30px 30px 30px;
       }
       p {
         font-size: 14px;
@@ -332,7 +408,7 @@ header {
   }
 
   .sec-2 {
-    figure {
+    .imgContainer {
       order: 2;
     }
   }
@@ -356,7 +432,7 @@ header {
   .sec-4 .row-grid {
     grid-template-columns: 1fr;
     align-items: center;
-    figure {
+    .imgContainer {
       order: 2;
     }
     article {
@@ -372,6 +448,50 @@ header {
         }
       }
     }
+  }
+
+  .sec-img {
+    max-width: 400px;
+    display: block;
+    margin: auto;
+    padding-bottom: 50px;
+  }
+}
+
+@media (max-width: 576px) {
+  .sec-img {
+    max-width: none;
+    display: block;
+    margin: auto;
+    padding-bottom: 0px;
+  }
+}
+</style>
+<style lang="scss">
+#aboutTimeline {
+  width: 100%;
+  .ant-timeline-item-tail {
+    border-color: $lightGrey;
+  }
+  .label {
+    color: $red;
+    font-weight: bold;
+    margin-bottom: 5px;
+    font-size: 16px;
+    margin: 0px 5px;
+  }
+  .text {
+    margin: 0px 5px;
+    font-size: 16px;
+    margin-bottom: 0px;
+    color: $grey;
+  }
+  .dot .circle {
+    background-color: $red;
+    height: 15px;
+    width: 15px;
+    // padding: 0px 10px;
+    border-radius: 50%;
   }
 }
 </style>
