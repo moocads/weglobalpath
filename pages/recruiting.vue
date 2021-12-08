de48c2c0b08eb352e59b8a013cbba5242b2bed5a
GaryZ <0619cloudz@gmail.com>
2021-12-08 12:51:15 -0500

    working on recruiting page

--- pages/recruiting.vue
+++ pages/recruiting.vue
@@ -51,9 +51,46 @@
         <div class="wrapper">
           <MainTitle title="申请要求" titleEN="REQUIREMENT" />
           <div class="requirement-grid">
-            <div class="table"></div>
-            <div class="content"></div>
+            <div class="table">
+              <a-table
+                :columns="columns"
+                :data-source="data"
+                :pagination="false"
+              >
+                <template slot="title"> 国际留学生学费与入学要求 </template>
+                <template slot="name" slot-scope="text">
+                  <span> {{ text }}</span>
+                </template>
+
+                <!-- <a slot="name" slot-scope="text">{{ text }}</a> -->
+              </a-table>
+            </div>
           </div>
+        </div>
+      </section>
+      <section class="plan-sec">
+        <div class="wrapper plan-content">
+          <figure>
+            <img src="/img/Recruiting/pdf-img.png" alt="" />
+          </figure>
+          <article>
+            <h2>加拿大罗伯森学院在中国</h2>
+            <p>
+              目前，加拿大罗伯森学院在中国区合作学校以达24所。近些年来与中国合作院校保持着长期良好的合作关系。
+            </p>
+            <p>
+              罗伯森学院在医疗保健，商业，社会工作，法律，旅游和技术等领域提供30多种以职业为中心的证书和大专文凭。课程由具有多年该领域经验的行业专业人士授课。
+              罗伯森还设有18个月中国加拿大双认证大专学历，毕业后年即可获得3年加拿大工作签证并申请加拿大永居身份，首年年薪可高达30万人民币。
+            </p>
+            <a
+              href="/img/Recruiting/plan.pdf"
+              class="pdf-download-btn"
+              download="罗伯森学院中国区招生计划完整版"
+            >
+              <img src="/img/Recruiting/download-icon.svg" alt="" />
+              <span>下载罗伯森学院中国区招生计划完整版</span>
+            </a>
+          </article>
         </div>
       </section>
     </main>
@@ -61,9 +98,98 @@
 </template>
 
 <script>
-export default {};
+const columns = [
+  {
+    dataIndex: "name",
+    key: "name",
+    scopedSlots: { customRender: "name" },
+  },
+  {
+    dataIndex: "requirement",
+    key: "requirement",
+    scopedSlots: { customRender: "name" },
+  },
+];
+const data = [
+  {
+    key: "1",
+    name: "专业课学费",
+    requirement: "16000加币 - 21000加币/年\n（根据专业不同）",
+  },
+  {
+    key: "2",
+    name: "语言预科学费",
+    requirement: "3500加币/3个月",
+  },
+  {
+    key: "3",
+    name: "学历成绩要求",
+    requirement: "高中及同等学历以上\n学术课平均成绩70%以上",
+  },
+  {
+    key: "4",
+    name: "语言要求",
+    requirement: "无语言成绩要求\n（双录取，可就读语言课程）",
+  },
+  {
+    key: "5",
+    name: "入学时间",
+    requirement: "春季、秋季、冬季",
+  },
+  {
+    key: "6",
+    name: "资金担保",
+    requirement: "根据加拿大签证政策最低只需购买1万加币GIC证明",
+  },
+];
+export default {
+  data() {
+    return {
+      data,
+      columns,
+    };
+  },
+};
 </script>
+<style lang="scss">
+#recruiting-page {
+  .ant-table {
+    max-width: 800px;
+  }
+  .ant-table-title {
+    font-size: 24px;
+    font-weight: bold;
+    text-align: center;
+    color: #fff;
+    letter-spacing: 2px;
+    background: $red;
+  }
+  .ant-table-thead {
+    display: none;
+  }
+  .ant-table-content {
+    border-left: 1px solid #e8e8e8;
+    border-right: 1px solid #e8e8e8;
+  }
+  .ant-table-row {
+    font-size: 18px;
+    font-weight: bold;
+    text-align: center;
+    white-space: pre;
+    td {
+      text-align: center;
+      background-color: #fff;
+    }
 
+    td:last-child {
+      background-color: #fafafa;
+      font-weight: normal;
+    }
+  }
+}
+@media all and (max-width: 1000px) {
+}
+</style>
 <style lang="scss" scoped>
 main {
   padding: 100px 0;
@@ -182,14 +308,62 @@ header {
   span {
     color: #ab9269;
   }
+  img {
+    max-height: 361px;
+    width: auto;
+  }
 }
 .requirement-sec {
   padding: 100px 0;
 }
 .requirement-grid {
   display: grid;
-  grid-template-columns: 1fr 1fr;
+  grid-template-columns: 1fr;
 }
+.table {
+  display: flex;
+  justify-content: center;
+  align-items: center;
+}
+.plan-sec {
+  background-color: $navy;
+  padding: 60px 0;
+}
+.plan-content {
+  display: flex;
+  column-gap: 75px;
+  color: #fff;
+  article {
+    display: flex;
+    flex-direction: column;
+  }
+  h2 {
+    color: #fff;
+    font-size: 28px;
+    font-weight: bold;
+    margin-bottom: 30px;
+  }
+  p {
+    font-size: 16px;
+  }
+  .pdf-download-btn {
+    margin-top: 35px;
+    max-width: 350px;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    font-size: 16px;
+    color: $red;
+    padding: 8px 15px;
+    background-color: #dadada;
+    border: 1px solid $red;
+    border-radius: 5px;
+    img {
+      margin-right: 9px;
+    }
+  }
+}
+
 @media all and (max-width: 600px) {
   header {
     height: 300px;
@@ -208,6 +382,15 @@ header {
       height: auto;
       width: 100%;
     }
+  }
+  .certificate-sec .content {
+    width: 100%;
+    img {
+      width: 100%;
+    }
+  }
+  .plan-content {
+    flex-direction: column;
   }
 }
 </style>
