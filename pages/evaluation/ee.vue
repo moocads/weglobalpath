0018716b740da17a783d5c600dec62cf1bd332e4
Rui Wang-Dong <rtdong9421@gmail.com>
2022-02-01 16:37:28 -0500

    ee form basic info

--- pages/evaluation/ee.vue
+++ pages/evaluation/ee.vue
@@ -12,11 +12,11 @@
     >
       <div class="wrapper">
         <h1>加拿大移民快速通道（EE）评分系统</h1>
-        <h2>Points: {{Object.values(totalPoints).reduce((a,b) => a + b) + ageCalc}}</h2>
       </div>
     </header>
     <div class="wrapper">
       <a-card>
+        <h2>Points: {{subtotal}}</h2>
         <div class="form">
           <div class="section">
             <div class="box last">
@@ -35,8 +35,10 @@
               </div>
             </div>
           </div>
-          <h3>1、基础信息</h3>
-          <div class="section" v-if="marriage === 'married'">
+          <br>
+          <br>
+          <div class="section">
+            <h3>1、基础信息</h3>
             <div class="box">
               <div class="label">
                 年龄
@@ -53,84 +55,44 @@
                 学历
               </div>
               <div class="question vertical-radio">
-                <a-radio-group v-model="totalPoints.education" >
-                  <a-radio :value="0">
+                <a-radio-group v-model="edu" >
+                  <a-radio :value="1">
                     初中以下
                   </a-radio>
-                  <a-radio :value="28">
+                  <a-radio :value="2">
                     高中
                   </a-radio>
-                  <a-radio :value="84">
+                  <a-radio :value="3">
                     1年大专
                   </a-radio>
-                  <a-radio :value="91">
+                  <a-radio :value="4">
                     2年大专
                   </a-radio>
-                  <a-radio :value="112">
+                  <a-radio :value="5">
                     3年以上大专或本科
                   </a-radio>
-                  <a-radio :value="119">
+                  <a-radio :value="6">
                     双专业 (3年以上 + 1年以上)
                   </a-radio>
-                  <a-radio :value="126">
+                  <a-radio :value="7">
                     硕士学位或专业学位（如医学博士）
                   </a-radio>
-                  <a-radio :value="140">
+                  <a-radio :value="8">
                     博士学位
                   </a-radio>
                 </a-radio-group>
               </div>
               <div class="result">
-                {{totalPoints.education}}
+                {{eduCalc}}
               </div>
             </div>
-          </div>
-          <div class="section" v-if="marriage === 'single'">
-            <div class="box">
-              <div class="label">
-                年龄
-              </div>
+            <div class="box last">
+              <div class="label">工作经验</div>
               <div class="question">
-                <a-input-number v-model="age" :min="1" :max="100"></a-input-number>
+                加拿大工作经验（可以不连续）<a-input-number v-model="exp"></a-input-number> 年
               </div>
               <div class="result">
-                {{ageCalc}}
-              </div>
-            </div>
-            <div class="box">
-              <div class="label">
-                学历
-              </div>
-              <div class="question vertical-radio">
-                <a-radio-group v-model="totalPoints.education" >
-                  <a-radio :value="0">
-                    初中以下
-                  </a-radio>
-                  <a-radio :value="30">
-                    高中
-                  </a-radio>
-                  <a-radio :value="90">
-                    1年大专
-                  </a-radio>
-                  <a-radio :value="98">
-                    2年大专
-                  </a-radio>
-                  <a-radio :value="120">
-                    3年以上大专或本科
-                  </a-radio>
-                  <a-radio :value="128">
-                    双专业 (3年以上 + 1年以上)
-                  </a-radio>
-                  <a-radio :value="135">
-                    硕士学位或专业学位（如医学博士）
-                  </a-radio>
-                  <a-radio :value="150">
-                    博士学位
-                  </a-radio>
-                </a-radio-group>
-              </div>
-              <div class="result">
-                {{totalPoints.education}}
+                {{expCalc}}
               </div>
             </div>
           </div>
@@ -144,11 +106,10 @@
 export default {
   data() {
     return {
-      marriage: undefined,
-      age: undefined,
-      totalPoints: {
-        education: undefined,
-      }
+      marriage: "single",
+      age: 0,
+      exp: 0,
+      edu: 0,
     }
   },
   methods: {
@@ -159,6 +120,7 @@ export default {
   },
   computed: {
     ageCalc: function() {
+      this.age = Math.round(this.age)
       if (this.marriage == "married") {
         return (
           this.age == 18 ? 90 :
@@ -180,8 +142,7 @@ export default {
           this.age == 43 ? 15 :
           this.age == 44 ? 5 : 0
         )
-      }
-      if (this.marriage == "single") {
+      } else if (this.marriage == "single") {
         return (
           this.age == 18 ? 99 :
           this.age == 19 ? 105 :
@@ -202,7 +163,66 @@ export default {
           this.age == 43 ? 17 :
           this.age == 44 ? 6 : 0
         )
+      } else {
+        return 0
       }
+    },
+    eduCalc: function() {
+      this.edu = Math.round(this.edu)
+      if (this.marriage == "married") {
+        return (
+          this.edu == 1 ? 0 :
+          this.edu == 2 ? 28 :
+          this.edu == 3 ? 84 :
+          this.edu == 4 ? 91 :
+          this.edu == 5 ? 112 :
+          this.edu == 6 ? 119 :
+          this.edu == 7 ? 126 :
+          this.edu == 8 ? 140 : 0
+        )
+      } else if (this.marriage == "single") {
+        return (
+          this.edu == 1 ? 0 :
+          this.edu == 2 ? 30 :
+          this.edu == 3 ? 90 :
+          this.edu == 4 ? 98 :
+          this.edu == 5 ? 120 :
+          this.edu == 6 ? 128 :
+          this.edu == 7 ? 135 :
+          this.edu == 8 ? 150 : 0
+        )
+      } else {
+        return 0
+      }
+    },
+    expCalc: function() {
+      this.exp = Math.round(this.exp)
+      if (this.marriage == "married") {
+        return (
+          this.exp < 1 ? 0 :
+          this.exp == 1 ? 35 :
+          this.exp == 2 ? 46 :
+          this.exp == 3 ? 56 :
+          this.exp == 4 ? 63 :
+          this.exp > 5 ? 70 : 0
+        )
+      } else if (this.marriage == "single") {
+        return (
+          this.exp < 1 ? 0 :
+          this.exp == 1 ? 40 :
+          this.exp == 2 ? 53 :
+          this.exp == 3 ? 64 :
+          this.exp == 4 ? 72 :
+          this.exp > 5 ? 80 : 0
+        )
+      } else {
+        return 0
+      }
+    },
+    subtotal: function() {
+      return (
+        this.ageCalc + this.eduCalc + this.expCalc
+      )
     }
   }
 };
@@ -238,6 +258,7 @@ export default {
 
     .question {
       padding: 20px 30px;
+      flex-grow: 1;
 
       .ant-radio-wrapper {
         margin-right: 30px;
@@ -251,7 +272,13 @@ export default {
     }
 
     .result {
+      padding: 20px 30px;
       width: 100px;
+      border-left: 1px solid #efefef;
+      display: flex;
+      flex-direction: column;
+      justify-content: center;
+      align-items: center;
     }
   }
 }
