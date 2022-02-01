656e75c1a926d00ee70974f6869b1679f68722ce
GaryZ <0619cloudz@gmail.com>
2022-02-01 10:54:09 -0500

    ADD: - 添加打分页面

---
+++ pages/evaluation/ee.vue
@@ -0,0 +1,24 @@
+<template>
+  <div>
+    <header
+      class="general-header"
+      :style="{
+        background: `linear-gradient(
+        89.93deg,
+        rgba(34, 52, 92, 0.7) 7.82%,
+        rgba(34, 52, 92, 0) 102.35%
+        ),url('/img/evaluation/evaluation-header.jpg')`,
+      }"
+    >
+      <div class="wrapper">
+        <h1>加拿大移民快速通道（EE）评分系统</h1>
+      </div>
+    </header>
+  </div>
+</template>
+
+<script>
+export default {};
+</script>
+
+<style lang="scss" scoped></style>
