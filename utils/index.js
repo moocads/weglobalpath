36224ca961d8a913ef7e2945bb197161dfd046cb
Aria <ariaqin1996@gmail.com>
2022-05-30 16:57:51 -0400

    save progress

--- utils/index.js
+++ utils/index.js
@@ -1,5 +1,12 @@
 import hotProjectsBreadcrumb from "./hotProjectsBreadcrumb";
 import hotProjectsNavMap from "./hotProjectsNavMap";
 import provincesMap from "./provincesMap";
+import { portugal28wFee, portugal50wFee } from "./euroFee";
 
-export { hotProjectsBreadcrumb, hotProjectsNavMap, provincesMap };
+export {
+  hotProjectsBreadcrumb,
+  hotProjectsNavMap,
+  provincesMap,
+  portugal28wFee,
+  portugal50wFee,
+};
